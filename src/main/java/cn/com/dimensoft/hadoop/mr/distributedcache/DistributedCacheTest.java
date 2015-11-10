95bf664b5dd9cbbc1ace2f7f8f79bcfefa7f9ced
zxh <16724105@qq.com>
2015-11-10 14:20:45 +0800

    custom inputformat

---
+++ src/main/java/cn/com/dimensoft/hadoop/mr/distributedcache/DistributedCacheTest.java
@@ -0,0 +1,129 @@
+package cn.com.dimensoft.hadoop.mr.distributedcache;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+/**
+ * 
+ * class： Template
+ * package： cn.com.dimensoft.hadoop.mr.distributedcache
+ * author：zxh
+ * time： 2015年11月10日 下午2:12:50
+ * description：
+ */
+public class DistributedCacheTest extends Configured implements Tool {
+
+	/**
+	 * 
+	 * name：main
+	 * author：zxh
+	 * time：2015年11月10日 下午2:13:45
+	 * description：
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		
+		System.exit(ToolRunner.run(new DistributedCacheTest(), args));
+	}
+	
+	
+	@Override
+	public int run(String[] args) throws Exception {
+
+//		get configuration
+		Configuration conf = this.getConf();
+		
+//		create job
+		Job job = Job.getInstance(conf);
+		
+//		set jar
+		job.setJarByClass(DistributedCacheTest.class);
+		
+//		set Mapper
+		job.setMapperClass(MyMapper.class);
+		
+//		set map output key/value
+		job.setMapOutputKeyClass(IntWritable.class);
+		job.setMapOutputValueClass(Text.class);
+		
+		/**		shuffle start	**/
+//		set patition
+//		job.setPartitionerClass(HashPartitioner.class);
+
+//		set sort comparator
+//		job.setSortComparatorClass(IntWritable.Comparator.class);
+		
+//		set combiner
+//		job.setCombinerClass(MyReducer.class);
+		
+//		set group comparator
+//		job.setGroupingComparatorClass(IntWritable.Comparator.class);
+		/**		shuffle end	**/
+		
+//		set Reducer
+		job.setReducerClass(MyReducer.class);
+		
+//		set input path
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		
+//		set output path
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		return job.waitForCompletion(true) ? 0 : 1;
+		
+	}
+
+	
+	/**
+	 * 
+	 * class： MyMapper
+	 * package： cn.com.dimensoft.hadoop.mr.distributedcache
+	 * author：zxh
+	 * time： 2015年11月10日 下午2:13:52
+	 * description：
+	 */
+	public static class MyMapper extends Mapper<LongWritable, Text, IntWritable, Text>{
+
+		@Override
+		protected void map(LongWritable key, Text value, Context context)
+				throws IOException, InterruptedException {
+			
+			super.map(key, value, context);
+		}
+		
+	}
+	
+	/**
+	 * 
+	 * class： MyReducer
+	 * package： cn.com.dimensoft.hadoop.mr.distributedcache
+	 * author：zxh
+	 * time： 2015年11月10日 下午2:13:57
+	 * description：
+	 */
+	public static class MyReducer extends Reducer<IntWritable, Text, IntWritable, Text>{
+
+		@Override
+		protected void reduce(IntWritable key, Iterable<Text> values, Context context)
+				throws IOException, InterruptedException {
+			
+			super.reduce(key, values, context);
+		}
+		
+	}
+	
+}
